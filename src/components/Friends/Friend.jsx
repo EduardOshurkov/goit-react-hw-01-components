1f43546b2938fab3eaf4e4b956e61cf8e1eb6f7e
EduardOshurkov <drd2993@gmail.com>
2022-07-23 19:14:19 +0300

    saved

--- src/components/Friends/Friend.jsx
+++ src/components/Friends/Friend.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types';
-import styles from './Friend.module.css'
+import styles from './Friend.module.css';
 
 export const Friend = ({ avatar, name, isOnline, id }) => {
     return (
-  <li className={styles.friendBox}>
-           <span className={styles.status} style={{backgroundColor:  isOnline ? "green" : "red"}}></span>
-  <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <li className={styles.friendBox}>
+            <span className={styles.status} style={{ backgroundColor: true ? "green" : "green" }}>{isOnline}</span>
+            <img className="avatar" src={avatar} alt="User avatar" width="48" />
             <p className={styles.friendsName}>{name}</p>
         </li>
         
     )
-}
+};
 
 Friend.propTypes = {
     avatar: PropTypes.string,
