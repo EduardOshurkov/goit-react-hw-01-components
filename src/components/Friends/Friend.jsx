216f392cddbafad42ff9cd7fcc950c386f7f52a4
EduardOshurkov <drd2993@gmail.com>
2022-07-23 18:45:55 +0300

    saved

--- src/components/Friends/Friend.jsx
+++ src/components/Friends/Friend.jsx
@@ -4,9 +4,7 @@ import styles from './Friend.module.css'
 export const Friend = ({ avatar, name, isOnline, id }) => {
     return (
   <li className={styles.friendBox}>
-            <span
-        className={styles.status}
-        style={{ backgroundColor: isOnline ? "red" :"green" }}></span>
+           <span className={styles.status} style={{backgroundColor:  isOnline ? "green" : "red"}}></span>
   <img className="avatar" src={avatar} alt="User avatar" width="48" />
             <p className={styles.friendsName}>{name}</p>
         </li>
